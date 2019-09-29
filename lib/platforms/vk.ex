4269b851be5bc0631941593f9f97db236792fbb3
Ekaterina Vaartis <vaartis@cock.li>
2019-09-29 21:49:38 +0300

    More formatting

--- lib/platforms/vk.ex
+++ lib/platforms/vk.ex
@@ -1,23 +1,26 @@
 defmodule DevRandom.Platforms.VK do
   def random_post_or_posts do
-    Process.sleep(1000) # Just in case
+    # Just in case
+    Process.sleep(1000)
 
     group_id = Application.get_env(:dev_random_ex, :group_id)
 
-     # Check the suggested posts
-    {:ok, suggested_posts_query} = vk_req(
-      "wall.get",
-      %{
-        owner_id: -group_id,
-        filter: "suggests"
-      }
-    )
+    # Check the suggested posts
+    {:ok, suggested_posts_query} =
+      vk_req(
+        "wall.get",
+        %{
+          owner_id: -group_id,
+          filter: "suggests"
+        }
+      )
 
     post_count = suggested_posts_query["count"]
 
-    if post_count > 0 do # There are suggested posts
-      suggested_post = random_from(
-        %{
+    # There are suggested posts
+    if post_count > 0 do
+      suggested_post =
+        random_from(%{
           method_name: "wall.get",
           all_count: post_count,
           per_page_count: 100,
@@ -26,13 +29,18 @@ defmodule DevRandom.Platforms.VK do
             filter: "suggests"
           },
           first_page: suggested_posts_query["items"]
-        }
-      )
-      suggested_post_id = suggested_post["id"] # Select the random post
+        })
+
+      # Select the random post
+      suggested_post_id = suggested_post["id"]
 
       # Check if post has attachments and there are images in them
-      if Map.has_key?(suggested_post, "attachments") and (suggested_post["attachments"] |> Enum.filter(fn att -> att["type"] in ["photo", "doc"] end) |> Enum.count) > 0  do
-        {:suggested, suggested_post} # Return the whole post
+      if Map.has_key?(suggested_post, "attachments") and
+           suggested_post["attachments"]
+           |> Enum.filter(fn att -> att["type"] in ["photo", "doc"] end)
+           |> Enum.count() > 0 do
+        # Return the whole post
+        {:suggested, suggested_post}
       else
         # Delete the post and restart if there's no attachments
         delete_suggested_post(suggested_post_id)
@@ -41,15 +49,16 @@ defmodule DevRandom.Platforms.VK do
         random_post_or_posts()
       end
     else
-      {:ok, members_query} = vk_req(
-        "groups.getMembers",
-        %{group_id: group_id}
-      )
+      {:ok, members_query} =
+        vk_req(
+          "groups.getMembers",
+          %{group_id: group_id}
+        )
 
       members_count = members_query["count"]
 
-      random_member = random_from(
-        %{
+      random_member =
+        random_from(%{
           method_name: "groups.getMembers",
           all_count: members_count,
           per_page_count: 1000,
@@ -57,30 +66,34 @@ defmodule DevRandom.Platforms.VK do
             group_id: group_id
           },
           first_page: members_query["items"]
-        }
-      )
+        })
 
       maybe_album_query = vk_req("photos.getAlbums", %{owner_id: random_member, need_system: 1})
+
       case maybe_album_query do
-        {:error, _}-> random_post_or_posts() # The user's account is probably deleted or something, just restart
+        # The user's account is probably deleted or something, just restart
+        {:error, _} ->
+          random_post_or_posts()
+
         {:ok, album_query} ->
           albums = album_query["items"]
 
           # There are saved photos
           if Enum.any?(albums, &(&1["id"] == -15)) do
-            {:ok, saved_photos_query} = vk_req(
-              "photos.get",
-              %{
-                owner_id: random_member,
-                album_id: "saved"
-              }
-            )
+            {:ok, saved_photos_query} =
+              vk_req(
+                "photos.get",
+                %{
+                  owner_id: random_member,
+                  album_id: "saved"
+                }
+              )
 
             saved_photos_count = saved_photos_query["count"]
 
             if saved_photos_count > 0 do
-              random_photo = random_from(
-                %{
+              random_photo =
+                random_from(%{
                   method_name: "photos.get",
                   all_count: saved_photos_count,
                   per_page_count: 1000,
@@ -89,8 +102,7 @@ defmodule DevRandom.Platforms.VK do
                     album_id: "saved"
                   },
                   first_page: saved_photos_query["items"]
-                }
-              )
+                })
 
               {:saved, random_photo}
             else
@@ -115,26 +127,26 @@ defmodule DevRandom.Platforms.VK do
   If VK returns one or unwrapping the returned `response`. If VK returns a 403 page,
   then the error will be reported as `{:error, :invalid_method}`
   """
-  @spec vk_req(method_name :: String.t, params :: map)
-  :: vk_result_t
+  @spec vk_req(method_name :: String.t(), params :: map) ::
+          vk_result_t
   def vk_req(method_name, params) do
     token = Application.get_env(:dev_random_ex, :token)
 
     __MODULE__.RequestTimeAgent.before_request()
-    query_result = HTTPoison.get!(
-      "https://api.vk.com/method/#{method_name}",
-      [],
-      [
+
+    query_result =
+      HTTPoison.get!(
+        "https://api.vk.com/method/#{method_name}",
+        [],
         params: Map.merge(params, %{access_token: token, v: "5.73"})
-      ]
-    )
+      )
 
     if query_result.status_code == 403 do
       # VK doesnt actually return an error on invalid method, although it should,
       # so we'll do it ourselves
       {:error, :invalid_method}
     else
-      result = Poison.decode! query_result.body
+      result = Poison.decode!(query_result.body)
 
       # Propagate the error if there is one
       if Map.has_key?(result, "error") do
@@ -154,48 +166,50 @@ defmodule DevRandom.Platforms.VK do
   was fetched to retreive the number of images before
   """
   @spec random_from(
-    params :: %{
-      method_name: String.t,
-      all_count: integer,
-      per_page_count: integer,
-      request_args: map,
-      first_page: [...]
-    }) :: term
-  defp random_from(
-    %{
-      method_name: method_name,
-      all_count: all_count,
-      per_page_count: per_page_count,
-      request_args: request_args,
-      first_page: first_page
-    }
-  ) do
-    Enum.random(if all_count > per_page_count do
-      total_pages = div(all_count, per_page_count)
+          params :: %{
+            method_name: String.t(),
+            all_count: integer,
+            per_page_count: integer,
+            request_args: map,
+            first_page: [...]
+          }
+        ) :: term
+  defp random_from(%{
+         method_name: method_name,
+         all_count: all_count,
+         per_page_count: per_page_count,
+         request_args: request_args,
+         first_page: first_page
+       }) do
+    Enum.random(
+      if all_count > per_page_count do
+        total_pages = div(all_count, per_page_count)
 
-      get_page = fn page ->
-        {:ok, current_page} = vk_req(
-        method_name,
-        Map.merge(%{count: per_page_count, offset: page * per_page_count}, request_args))
+        get_page = fn page ->
+          {:ok, current_page} =
+            vk_req(
+              method_name,
+              Map.merge(%{count: per_page_count, offset: page * per_page_count}, request_args)
+            )
 
-        current_page["items"]
+          current_page["items"]
+        end
+
+        # Use the first 100 entities as the beginning, then collect other pages begging from
+        # the second one (there must me at least two pages if there's more then `per_page_count` entities)
+        first_page ++ Enum.flat_map(2..total_pages, &get_page.(&1))
+      else
+        # Just return what we already had
+        first_page
       end
-
-
-      # Use the first 100 entities as the beginning, then collect other pages begging from
-      # the second one (there must me at least two pages if there's more then `per_page_count` entities)
-      first_page ++ Enum.flat_map(2..total_pages, &get_page.(&1))
-    else
-      # Just return what we already had
-      first_page
-    end)
+    )
   end
 
   @doc """
   Deletes the specified post, returning the request result.
   """
-  @spec delete_suggested_post(post_id :: integer)
-  :: vk_result_t
+  @spec delete_suggested_post(post_id :: integer) ::
+          vk_result_t
   def delete_suggested_post(post_id) do
     group_id = Application.get_env(:dev_random_ex, :group_id)
 
