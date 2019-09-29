683e97cb42bd1d4f76cfe31aa841081a884d38ad
Ekaterina Vaartis <vaartis@cock.li>
2019-09-29 22:08:16 +0300

    Use the new posting helper function in scheduler

--- config/config.exs
+++ config/config.exs
@@ -13,7 +13,7 @@ config :dev_random_ex,
   # log_file: "dev_random.log"
 config :dev_random_ex, DevRandom.Scheduler,
   jobs: [
-    {"*/30 * * * *", {GenServer, :cast, [DevRandom, :post]}}
+    {"*/30 * * * *", {DevRandom, :post, []}}
   ]
 
 config :pid_file, file: "./dev_random_ex.pid"
